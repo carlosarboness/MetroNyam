47034f6b39349ce251c15ef5ef380ad6b01575ff
carlosarboness <102261662+carlosarboness@users.noreply.github.com>
2022-04-25 18:25:23 +0200

    Add files via upload

--- restaurants.py
+++ restaurants.py
@@ -1,10 +1,27 @@
-@dataclass 
-class Restaurant: ...
 
-Restaurants = List[Restaurant]
+import string
 
 
-def read() -> Restaurants: ...
+class Restaurant:
+
+    _id : int
+    _name : string
+    _adress: string
+    _neighbourhood: string
+    _tel : int                 
+
+    def __init__(self, id: int, name: string, adress: string, neighbourhood: string, tel: int) -> None:
+        _id = id
+        _name = name
+        _adress = adress
+        _neighbourhood = neighbourhood
+        _tel = tel
+
+Restaurants = list[Restaurant]
 
 
-def find(query: str, restaurants: Restaurants) -> Restaurants: ...
+def read() -> Restaurants: 
+    return 
+
+def find(query: str, restaurants: Restaurants) -> Restaurants: 
+    return
