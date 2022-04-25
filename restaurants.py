2a125ebed6739cf73400bd55f5cd86a9c68874b4
benetraco <101699331+benetraco@users.noreply.github.com>
2022-04-25 20:32:19 +0200

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -1,27 +1,40 @@
-
-import string
+from typing import Optional, List, Tuple
 
 
 class Restaurant:
 
-    _id : int
-    _name : string
-    _adress: string
-    _neighbourhood: string
-    _tel : int                 
+    _id: int
+    _name: str
+    _adress: Tuple[str, int]
+    _neighbourhood: str
+    _district: str
+    _zip_code: int
+    _tel: int
 
-    def __init__(self, id: int, name: string, adress: string, neighbourhood: string, tel: int) -> None:
+    def __init__(self, id: int, name: str, adress: Tuple[str, int], neighbourhood: str, district: str, zip_code: int, tel: int) -> None:
         _id = id
         _name = name
         _adress = adress
         _neighbourhood = neighbourhood
+        _district = district
+        _zip_code = zip_code
         _tel = tel
 
-Restaurants = list[Restaurant]
+
+Restaurants = List[Restaurant]
 
 
-def read() -> Restaurants: 
-    return 
-
-def find(query: str, restaurants: Restaurants) -> Restaurants: 
+def read() -> Restaurants:
     return
+
+
+def coincidence(query: str, res: Restaurant) -> bool:
+    return query.lower() == (str(res._id) or res._adress[0].lower() or res._neighbourhood.lower() or res._district.lower() or str(res._zip_code))
+
+
+def find(query: str, restaurants: Restaurants) -> Restaurants:
+    restaurants_query: Restaurants = []
+    for restaurant in restaurants:
+        if coincidence(query, restaurant):
+            restaurants_query.append(restaurant)
+    return restaurants_query
