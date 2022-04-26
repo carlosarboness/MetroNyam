64f28cfe3ece144da39e1538b64126e587eab82b
benetraco <101699331+benetraco@users.noreply.github.com>
2022-04-27 00:11:57 +0200

    Versió amb intent de llegir dues paraules

--- restaurants.py
+++ restaurants.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from staticmap import StaticMap, CircleMarker
 from typing import Optional, List, Tuple
@@ -29,17 +28,22 @@ class Restaurant:
     def get_adress(self) -> Tuple[str, int]: 
         return self._adress
     
+    def get_neighborhood(self) -> str:
+        return self._neighborhood
+    
     def get_district(self) -> str: 
         return self._district
     
-    def get_tel(self) -> int: 
-        return self._tel
-        
+    def get_zip_code(self) -> int: 
+        return self._zip_code
+
+
 Restaurants = List[Restaurant]
 
 def read() -> Restaurants:
     #http://www.bcn.cat/tercerlloc/files/restaurants/opendatabcn_restaurants_restaurants-csv.csv
     df = pd.read_csv('rest.csv')
+    df = df.astype(str) #we pass the df readed into a str
     Restaurants_list: List[Restaurant] = []
     for index, row in df.iterrows():
         r = Restaurant(row['register_id'], row['name'], (row['addresses_road_name'], row['addresses_start_street_number']), 
@@ -48,12 +52,24 @@ def read() -> Restaurants:
     return Restaurants_list
    
 def coincidence(query: str, res: Restaurant) -> bool:
-    for word in res._name.split():
-    #if res._name is a string with more than one word, we use de function split to
-    #divide it into a list of strings (with only one word) to be able to compare it
-        if query.lower() == word.lower():
-            return True
-    return query.lower() == (str(res._id) or res._adress[0].lower() or res._neighbourhood.lower() or res._district.lower() or str(res._zip_code))
+    query = query.lower()
+    lst_query: list[str] = []
+    for w_query in query.split():
+        lst_query.append(w_query)
+    for w_query in lst_query:
+        in_name: bool = False
+        for w_name in res.get_name().split():
+        #if res._name is a string with more than one word, we use de function split to
+        #divide it into a list of strings (with only one word) to be able to compare it
+            if w_query == w_name.lower():
+                in_name = True
+        adress: str = res.get_adress()[0].lower()
+        neighborhood: str = res.get_neighborhood().lower()
+        district: str = res.get_district().lower()
+        zip_code: str = res.get_zip_code().lower()
+        if not in_name and w_query != adress and w_query != neighborhood and w_query != district and w_query != zip_code:
+            return False
+    return True
 
 def find(query: str, restaurants: Restaurants) -> Restaurants:
     restaurants_query: Restaurants = []
@@ -66,8 +82,9 @@ def exec() -> None:
     restaurants = read()
     for rest in restaurants: 
         print(rest._district)
-    query = "Eixample"
+    query = "Sushi"
     filter = find(query, restaurants)
-    print(len(filter))
+    for rest in filter:
+        print(rest._name, rest._neighborhood, rest._district)
     
 exec()
