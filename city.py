621327ef7b9055606c661fd2abff2faad8811890
carlosarboness <102261662+carlosarboness@users.noreply.github.com>
2022-05-03 19:45:27 +0200

    Add files via upload

--- city.py
+++ city.py
@@ -1,4 +1,5 @@
 
+from dis import show_code
 import osmnx as ox 
 from attr import attributes
 import pandas as pd
@@ -30,16 +31,48 @@ def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
 def load_osmnx_graph(filename: str) -> OsmnxGraph: 
     return ox.load_graphml(filename)
 
+def add_access_streets(g: CityGraph) -> None:
+    dist = float('inf')
+    n1 = ""
+    n2 = ""
+    for access in g.nodes(data=True):
+        if access[1]['type'] == 'Access':
+            for street in g.nodes(data=True):
+                if street[1]['type'] == 'Street' and haversine(street[1]['pos'], access[1]['pos']) < dist:
+                    dist = haversine(street[1]['pos'], access[1]['pos'])
+                    n1 = street[1]
+                    n2 = access[1]
+        att4 = {
+            'type': 'Street',
+            #'length': haversine(n1[1]['pos'], n2[1]['pos'])
+        }
+        g.add_edge(n1, n2, **att4)
+                    
+
 def build_city_graph(g1: OsmnxGraph, g2: MetroGraph) -> CityGraph: 
-    g = CityGraph = CityGraph()
-    for n1 in g1.nodes(): 
-        g.add_node(n1)
-    for n2 in g2.nodes(): 
-        g.add_node(n2)
-    for e1 in g1.edges(): 
-        g.add_edge(e1)
-    for e2 in g2.edges(): 
-        g.add_edge(e2)
+    g : CityGraph = CityGraph()
+    for n1 in g1.nodes.data(): 
+        att = {
+            'type': 'Street', 
+            'pos': (n1[1]['x'], n1[1]['y'])
+        }
+        g.add_node(n1[0], **att)
+    for n2 in g2.nodes.data(): 
+        g.add_node(n2[0], **n2[1])
+    for e1 in g1.edges.data(): 
+        if e1[0] != e1[1]: 
+            att1 = {
+                'type': 'Street',
+                'length': e1[2]['length']
+            }
+            g.add_edge(e1[0], e1[1], **att1)
+    for e2 in g2.edges.data(): 
+        if e2[0] != e2[1]: 
+            g.add_edge(e2[0], e2[1], **e2[2])
+
+    add_access_streets(g)
+
+    return g
 
 def exec() -> None: 
     g1 = get_osmnx_graph()
