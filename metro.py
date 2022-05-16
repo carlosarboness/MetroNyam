e56d9a726ce08dbae51c50232c356d88672d50f1
benetraco <101699331+benetraco@users.noreply.github.com>
2022-05-16 09:21:41 +0200

    Update metro.py

--- metro.py
+++ metro.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
 from typing_extensions import TypeAlias
-from os import access
-from attr import attributes
 import pandas as pd
 import networkx as nx
 from staticmap import *
@@ -12,16 +10,16 @@ from PIL import Image
 import PIL
 
 
-location = Tuple[float, float]
+location = Tuple[float, float]  # (latitude, longitude)
 
 
 @dataclass
 class Station:
-    _station_code: str  # CODI_ESTACIO
-    _name: str  # NOM_ESTACIO
-    _line: str  # NOM_LINIA
-    _color: str  # COLOR_LINIA
-    _location: location  # GEOMETRY -with modifications-
+    _station_code: str  # station's code
+    _name: str  # station's name
+    _line: str  # station's line
+    _color: str  # station's color
+    _location: location  # station's location
 
     def get_station_code(self) -> str:
         return self._station_code
@@ -41,10 +39,10 @@ class Station:
 
 @dataclass
 class Access:
-    _name: str  # NOM_ACCES
-    _station_code: str  # ID_ESTACIO
-    _station_name: str  # NOM_ESTACIO
-    _location: location  # GEOMETRY -with modifications-
+    _name: str  # access' name
+    _station_code: str  # access' station code
+    _station_name: str  # access' station name
+    _location: location  # access' location
 
     def get_name(self) -> str:
         return self._name
@@ -70,10 +68,13 @@ MetroGraph: TypeAlias = nx.Graph
 
 def point(geomety: str) -> location:
     """Returns the location from a 'geometry' string
-    Prec: geometry must be a string with the following format: 'POINT (longitud latitud)' """
-    
+    Prec: geometry must be a string with the following
+    format: 'POINT (longitud latitud)' """
+
     word_lst: list[str] = geomety.split()
-    return (float(word_lst[1].replace('(', '')), float(word_lst[2].replace(')', '')))
+    latitude: float = float(word_lst[1].replace('(', ''))
+    longitude: float = float(word_lst[2].replace(')', ''))
+    return (latitude, longitude)
 
 
 def read_stations() -> Stations:
@@ -81,13 +82,14 @@ def read_stations() -> Stations:
     Prec: the file 'estacions.csv' must be downloanded"""
 
     df = pd.read_csv('estacions.csv')
-    df = df.astype(str)  # we pass the df readed into a strings to get the correct format of the data
+    # we pass the df readed into a str to get the correct format of the data
+    df = df.astype(str)
 
     Stat_lst: Stations = []
 
     for index, row in df.iterrows():
         stat = Station(row['CODI_ESTACIO'], row['NOM_ESTACIO'], row['NOM_LINIA'],
-                    row['COLOR_LINIA'], point(row['GEOMETRY']))
+                    '#' + row['COLOR_LINIA'], point(row['GEOMETRY']))
         Stat_lst.append(stat)
 
     return Stat_lst
@@ -98,7 +100,8 @@ def read_accesses() -> Accesses:
     Prec: the file 'accessos.csv' must be downloanded"""
 
     df = pd.read_csv('accessos.csv')
-    df = df.astype(str)  # we pass the df readed into a strings to get the correct format of the data
+    # we pass the df readed into a str to get the correct format of the data
+    df = df.astype(str)
 
     Access_lst: Accesses = []
 
@@ -123,15 +126,16 @@ def get_att_station(stat: Station) -> dict:
 
 
 def get_att_tram(stat1: Station, stat2: Station) -> dict:
-    """Returns a map of the attributes of the edge between the stations stat1 and stat2"""
+    """Returns a map of the attributes of the edge between
+    the stations stat1 and stat2"""
 
     attributes: dict = {
         'type': 'tram',
         'weight': float(haversine(stat2.get_location(), stat1.get_location()))*(1/30),
         'dist': haversine(stat2.get_location(), stat1.get_location()),
-        'speed': 26/(3.6), 
+        'speed': 26/(3.6),
         'line': stat1.get_line(),
-        'color': get_colors()[stat1.get_line()]
+        'color': stat1.get_color()
     }
     return attributes
 
@@ -153,8 +157,8 @@ def get_att_edge_access(access: Access, dist: float) -> dict:
     attributes: dict = {
             'type': 'Access',
             'weight': dist*(1/6),
-            'dist': dist, 
-            'speed': 6/(3.6), 
+            'dist': dist,
+            'speed': 6/(3.6),
             'line': 'Null',
             'color': 'black'  # the color black means that it is walking
         }
@@ -169,7 +173,7 @@ def get_att_link(dist: float) -> dict:
         'type': 'Link',
         'weight': dist*(1/6),
         'dist': dist,
-        'speed': 6/(3.6), 
+        'speed': 6/(3.6),
         'line': 'Null',
         'color': 'black'  # the color black means that it is walking
     }
@@ -191,8 +195,8 @@ def get_node_access_name(access: Access) -> str:
 
 
 def add_station_node(metro: MetroGraph, stat: Station, stat_map: dict) -> None:
-    """Adds the node of the Station stat into the metro with its attrubites 
-    and into the stat_map"""
+    """Adds the node of the Station stat into the metro with
+    its attrubites and into the stat_map"""
 
     # add the node and its attributes to the metro
     att: dict = get_att_station(stat)
@@ -208,7 +212,8 @@ def add_station_node(metro: MetroGraph, stat: Station, stat_map: dict) -> None:
 
 
 def add_tram_edge(metro: MetroGraph(), stat1: Station, stat2: Station) -> None:
-    """Adds an edge into the metro between the node of the stations stat1 and stat2 with its attributes
+    """Adds an edge into the metro between the node of the stations
+    stat1 and stat2with its attributes
     Prec: the nodes of the stat1 and stat2 must be in the metro"""
 
     att: dict = get_att_tram(stat1, stat2)
@@ -226,18 +231,21 @@ def add_access_edge(metro: MetroGraph, access: Access) -> None:
     """Adds an edge in the metro between the access and its Station
     Prec: the nodes of the acces and its Station must be in the metro"""
 
-    # we first build the station name that has to connect with the access and the access name
+    # we first build the station name that has to connect
+    # with the access and the access name
     stat_name = access.get_station_name() + '-' + access.get_station_code()
     access_name = get_node_access_name(access)
 
-    # we get the attributes of the edge using the access and its distance to the node
+    # we get the attributes of the edge using the access
+    # and its distance to the node
     att: dict = get_att_edge_access(access, haversine(metro.nodes[stat_name]['pos'], metro.nodes[access_name]['pos']))
 
     metro.add_edge(stat_name, access_name, **att)
 
 
 def add_link_edge(metro: MetroGraph, lst_station: list) -> None:
-    """Given a list of stations, connects its nodes with link edges into the metro
+    """Given a list of stations, connects its nodes with link edges
+    into the metro
     Prec: the nodes of the stations must be in the metro"""
 
     for stat1 in lst_station:
@@ -249,8 +257,9 @@ def add_link_edge(metro: MetroGraph, lst_station: list) -> None:
 
 
 def add_station_nodes_and_edges(metro_grf: MetroGraph, lst_stations: Stations, stat_map: dict) -> None:
-    """Adds the station nodes to the metro_grf and the edges of the diferent metro lines and builds the
-    stat_map storing the stations with the same name but diferent lines in the same key"""
+    """Adds the station nodes to the metro_grf and the edges of the diferent
+    metro lines and builds the stat_map storing the stations with the same
+    name but diferent lines in the same key"""
 
     last_line: Optional[str] = None
     last_node: Optional[Station] = None
@@ -266,12 +275,14 @@ def add_station_nodes_and_edges(metro_grf: MetroGraph, lst_stations: Stations, s
         last_node = stat
 
 
-
 def build_metro_graph(lst_stations: Stations, lst_accesses: Accesses) -> MetroGraph:
     """Given a list of stations and accesses returns its Metrograph"""
 
     metro_grf = MetroGraph()
-    stat_map: dict = {}  # map that stores the stations with the same name but diferent lines in the same key
+
+    # map that stores the stations with the same name
+    # but diferent lines in the same key
+    stat_map: dict = {}
 
     add_station_nodes_and_edges(metro_grf, lst_stations, stat_map)
 
@@ -281,7 +292,7 @@ def build_metro_graph(lst_stations: Stations, lst_accesses: Accesses) -> MetroGr
 
     for key in stat_map:
         add_link_edge(metro_grf, stat_map[key])
-    
+
     return metro_grf
 
 
@@ -297,25 +308,16 @@ def get_metro_graph() -> MetroGraph:
 
 
 def show(g: MetroGraph) -> None:
-    """ Shows in an interactive screen the graph (nodes and edges) from g, that is the graph that contains 
-    the graph of the metro of Barcelona """
+    """ Shows in an interactive screen the graph (nodes and edges) from g,
+    that is the graph that contains the graph of the metro of Barcelona """
 
     nx.draw(g, nx.get_node_attributes(g, 'pos'), node_size=10, node_color="red", edge_color="blue", with_labels=False)
     plt.show()
 
 
-def get_colors() -> dict:
-    """Returns a map of with de keys being the names of the lines and values being its colors"""
-
-    colors : dict = {'Null': 'black', 'L1': 'red', 'L2': 'purple', 'L3': 'green', 
-                    'L4': 'yellow', 'L5': 'blue', 'L6': 'violet', 'L7': 'brown',
-                    'L8': 'pink', 'L9S': 'orange', 'L9N': 'orange', 'L10N': 'cyan', 
-                    'L10S': 'cyan', 'L11': 'lime', 'FM': 'white'}
-    return colors
-
-
 def paint_nodes(g: MetroGraph, m: StaticMap) -> None:
-    """ Paints all the nodes from the graph g in the StaticMap m with the color red """
+    """ Paints all the nodes from the graph g in the
+    StaticMap m with the color red """
 
     for index, node in g.nodes(data=True):
         coord = (node['pos'])
@@ -324,7 +326,8 @@ def paint_nodes(g: MetroGraph, m: StaticMap) -> None:
 
 
 def paint_edges(g: MetroGraph, m: StaticMap) -> None:
-    """ Paints all the edges from the graph g in the StaticMap m with the color blue """
+    """ Paints all the edges from the graph g in the
+    StaticMap m with the color blue """
 
     for n1 in g.edges(data=True):
         coord = (g.nodes[n1[0]]['pos'], g.nodes[n1[1]]['pos'])
@@ -332,9 +335,11 @@ def paint_edges(g: MetroGraph, m: StaticMap) -> None:
         line = Line(coord, color, 5)
         m.add_line(line)
 
+
 def plot(g: MetroGraph, filename: str) -> None:
-    """ Saves the image of the metrograph in the file -filename-. We iterate all the nodes, and  edges from the
-    graph g and colour them with the image of the barcelona map at the background """
+    """ Saves the image of the metrograph in the file -filename-.
+    We iterate all the nodes, and  edges from the graph g and
+    colour them with the image of the barcelona map at the background """
 
     url: str = 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
     m: StaticMap = StaticMap(1000, 1000, url_template=url)
